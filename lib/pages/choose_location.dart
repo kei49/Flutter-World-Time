37ef44df6b8b00dfa4c95756727ee1e87dbe49c3
Kei Shimizu <suns.manage.k@gmail.com>
2020-09-21 12:15:47 +0900

    Created world time app

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  List<WorldTime> locations = [
+    WorldTime(url: 'Asia/Tokyo', location: 'Tokyo', flag: 'japan.png', ),
+    WorldTime(url: 'Europe/Amsterdam', location: 'Amsterdam', flag: 'netherlands.png', ),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png', ),
+    WorldTime(url: 'America/Los_Angeles', location: 'Los Angeles', flag: 'usa.png', ),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png', ),
+    WorldTime(url: 'Asia/Singapore', location: 'Singapore', flag: 'singapore.png', ),
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png', ),
+    WorldTime(url: 'Europe/Rome', location: 'Rome', flag: 'italy.png', ),
+    WorldTime(url: 'Europe/Berlin', location: 'Berlin', flag: 'germany.png', ),
+    WorldTime(url: 'Europe/Athens', location: 'Athens', flag: 'greece.png', ),
+    WorldTime(url: 'Europe/Jakarta', location: 'Jakarta', flag: 'indonesia.png', ),
+    WorldTime(url: 'Europe/Seoul', location: 'Seoul', flag: 'south_korea.png', ),
+    WorldTime(url: 'Europe/Nairobi', location: 'Nairobi', flag: 'kenya.png', ),
+    WorldTime(url: 'Europe/Cairo', location: 'Cairo', flag: 'egypt.png', ),
+  ];
+
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+    await instance.getTime();
+    Navigator.pop(context, {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index){
+          return Card(
+            child: ListTile(
+              onTap: () {
+                updateTime(index);
+              },
+              title: Text(locations[index].location),
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/${locations[index].flag}'),
+              )
+            )
+          );
+        },
+      ),
+
+    );
+  }
+}
